89f03ec8f1d116d929237bd11280ca1579364e6e
Evlantiya <serfer.po.kiskam1337@gmail.com>
2024-06-14 01:39:24 +0500

    bugfix

--- Server/Repositories/LocalPatchRepostiory.cs
+++ Server/Repositories/LocalPatchRepostiory.cs
@@ -1,6 +1,7 @@
 using Newtonsoft.Json;
 using System.IO;
 using Microsoft.Extensions.Caching.Memory;
+using System.Globalization;
 
 public class LocalPatchRepositroy : IPatchRepository
 {
@@ -22,14 +23,14 @@ public class LocalPatchRepositroy : IPatchRepository
             // string name = patchJSON?.name?.Value ?? "undefined";
             string version = patchJSON.patchInfo.version.Value;
             string title = patchJSON.patchInfo.title.Value;
-            DateTime releaseDate = DateTime.Parse(patchJSON.patchInfo.releaseDate.Value);
+            DateTime releaseDate = DateTime.ParseExact(patchJSON.patchInfo.releaseDate.Value, "dd.MM.yyyy", CultureInfo.InvariantCulture);
             int weeksCount = (int) patchJSON.patchInfo.weeksCount.Value;
             List<EventDTO> events = new();
             var eventsInfo=patchJSON.patchInfo.events;
             foreach(var e in eventsInfo){
                 string eventTitle = e.eventTitle.Value;
-                DateTime startDate = DateTime.Parse(e.startDate.Value);
-                DateTime endDate = DateTime.Parse(e.endDate.Value);
+                DateTime startDate = DateTime.ParseExact(e.startDate.Value, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+                DateTime endDate = DateTime.ParseExact(e.endDate.Value, "dd.MM.yyyy", CultureInfo.InvariantCulture);
                 int stellarJadesAmount = (int) e.stellarJadesAmount.Value;
                 events.Add(new EventDTO(eventTitle, startDate, endDate, stellarJadesAmount));
             }
